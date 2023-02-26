2f0650fb328fec2f3098db060c990d7392e03f30
Mahmuddin Nurul Fajri <mahmuddinnf@gmail.com>
2023-02-26 14:19:28 +0700

    add margin top in login and register page

--- resources/js/components/Register.vue
+++ resources/js/components/Register.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container h-100">
+    <div class="container h-100 mt-4">
         <div class="row h-100 align-items-center">
             <div class="col-12 col-md-6 offset-md-3">
                 <div class="card shadow sm">
